f27100ce6bc682089568861affb9c54b732c8f16
Suyesh Bhatta <suyesh.bhatta199@gmail.com>
2024-05-05 18:47:01 -0500

    aaa

--- app/Models/available_services.php
+++ app/Models/available_services.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Available_Services extends Model
+
 {
     use HasFactory;
+    // print(;)
 
     protected $fillable = [
         'user_id',
